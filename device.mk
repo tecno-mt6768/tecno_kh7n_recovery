3b7f8b842c58fd95fe62031adea98d4f1c688942
isus203 <128573607+isus203@users.noreply.github.com>
2024-02-26 20:54:01 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -103,3 +103,5 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
