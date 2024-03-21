c9ddd46dc2735ef13a35c776ad71ef0bc5aa5fea
isus203 <128573607+isus203@users.noreply.github.com>
2024-03-21 13:45:49 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,7 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 # Storage
 RECOVERY_SDCARD_ON_DATA := true
-#BOARD_HAS_NO_REAL_SDCARD := true 
+BOARD_HAS_NO_REAL_SDCARD := true 
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 
