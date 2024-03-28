f31c5be3ba2156bcb4a84c4a5d0cef05006e2cf4
isus203 <128573607+isus203@users.noreply.github.com>
2024-03-28 08:04:15 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,7 +213,6 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 RECOVERY_SDCARD_ON_DATA := true
 #BOARD_HAS_NO_REAL_SDCARD := true 
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 
 # Device 
 TW_DEVICE_VERSION := Tecno Stark 9 Pro KH7n - isus203
