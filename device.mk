5ce720dcd8e6c0082aedb5b55a510e01f6d01e6f
isus203 <128573607+isus203@users.noreply.github.com>
2024-04-18 07:59:23 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,8 +21,6 @@ LOCAL_PATH := device/tecno/KH7n
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-PRODUCT_PLATFORM := mt6768
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
@@ -30,15 +28,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
+    gz \
     lk \
+    logo \
+    md1img \
     preloader \
     product \
+    scp \
+    spmfw \
+    sspm \
     system \
     system_ext \
+    tee \
     vbmeta \
     vbmeta_system \
     vbmeta_vendor \
