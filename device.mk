a8a01d5c7f7c4842f9c4c057a085e88d245f82d4
isus203 <128573607+isus203@users.noreply.github.com>
2024-04-18 10:24:27 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # A/B
 AB_OTA_PARTITIONS += \
