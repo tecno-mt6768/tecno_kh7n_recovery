0a93cebe096fbf2710172deb8b30e22b429c391b
isus203 <128573607+isus203@users.noreply.github.com>
2024-12-16 08:51:18 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ MTK_HARDWARE := true
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=fastboot
 BOARD_KERNEL_IMAGE_NAME := kernel
 BOARD_NAME := CY-KH7N-H6919
 BOARD_KERNEL_PAGESIZE := 2048
